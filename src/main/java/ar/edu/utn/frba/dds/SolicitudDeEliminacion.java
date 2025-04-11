315e002c138d928386dba152666846b376bfd624
Juan Tarducci <jtarducci@frba.utn.edu.ar>
2025-04-11 19:16:32 -0300

    agrego SolicitudDeEliminacion como clase
    Co-Authored-By: Fausto <faustoandresrodriguez@gmail.com>
    Co-Authored-By: JuanPabloMontemarani <165086575+juanpablomontemarani@users.noreply.github.com>
    Co-Authored-By: Manuel Rafael <manu44rafa44@gmail.com>
    Co-Authored-By: JuanFFernan05 <167038253+juanffernan05@users.noreply.github.com>

---
+++ src/main/java/ar/edu/utn/frba/dds/SolicitudDeEliminacion.java
@@ -0,0 +1,14 @@
+package ar.edu.utn.frba.dds;
+
+public class SolicitudDeEliminacion {
+    private String descripcion;
+    private Hecho hecho;
+    private boolean estado;
+
+    public void aceptar(){
+
+    }
+    public void rechazar(){
+
+    }
+}
