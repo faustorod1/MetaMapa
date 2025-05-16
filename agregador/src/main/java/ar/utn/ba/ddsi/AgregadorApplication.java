c11770797ec8d530a202ae37d3010fc60da761de
JuanFFernan05 <jfran0505@gmail.com>
2025-05-16 11:04:03 -0300

    Creación de módulos: agregador, fuenteE, fuenteD y fuenteP. Configuración de POMs

---
+++ agregador/src/main/java/ar/utn/ba/ddsi/AgregadorApplication.java
@@ -0,0 +1,12 @@
+package ar.utn.ba.ddsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgregadorApplication.class, args);
+    }
+
+}
