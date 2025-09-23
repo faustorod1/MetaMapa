7e0cc89c867e2f818d8539ef042c14e48c4f9a58
JuanFFernan05 <jfran0505@gmail.com>
2025-09-23 17:58:32 -0300

    cliente-grafico creado.

---
+++ cliente-grafico/src/main/java/ar/utn/ba/ddsi/ClienteGraficoApplication.java
@@ -0,0 +1,10 @@
+package ar.utn.ba.ddsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteGraficoApplication {
+    public static void main(String[] args) {SpringApplication.run(ClienteGraficoApplication.class, args);
+    }
+}
