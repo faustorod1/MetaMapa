1f34e89b74904a672fe7f2136de94b7f551690d2
JuanPabloMontemarani <jmontemarani@frba.utn.edu.ar>
2025-12-16 00:56:19 -0300

    Cambio en clienteGraficoApplication

--- servicio-frontend/src/main/java/ar/utn/ba/ddsi/ClienteGraficoApplication.java
+++ servicio-frontend/src/main/java/ar/utn/ba/ddsi/ClienteGraficoApplication.java
@@ -2,9 +2,14 @@ package ar.utn.ba.ddsi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class ClienteGraficoApplication {
     public static void main(String[] args) {SpringApplication.run(ClienteGraficoApplication.class, args);}
 }
